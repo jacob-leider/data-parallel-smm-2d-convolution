99a873a8408d5838ee65c4d0ac2969902e0c2b0c
Timothy Cronin <tcroniniv@gmail.com>
2024-04-22 12:47:28 -0400

    cleanup

--- gen_clangd.py
+++ gen_clangd.py
@@ -3,22 +3,17 @@ import pybind11
 import os
 
 
-def get_python_include():
-    python_include = subprocess.check_output(
-        ['python3-config', '--include']).decode().strip().split()
-    return python_include
-
-
-def create_clangd_file(file_path):
+def run(file_path):
     flags = [
-        '-Wall',
-        '-Wextra',
-        '-Werror',
-        '-std=c++17',
-        '-I' +
-        os.path.join(os.path.dirname(pybind11.__file__), 'include'),
-        *get_python_include()
-    ]
+            '-Wall',
+            '-Wextra',
+            '-Werror',
+            '-std=c++17',
+            '-I' +
+            os.path.join(os.path.dirname(pybind11.__file__), 'include'),
+            *subprocess.check_output(
+                ['python3-config', '--include']).decode().strip().split()
+            ]
 
     with open(file_path, 'w') as f:
         f.write('CompileFlags:\n')
@@ -28,4 +23,4 @@ def create_clangd_file(file_path):
 
 
 if __name__ == '__main__':
-    create_clangd_file('.clangd')
+    run('.clangd')
